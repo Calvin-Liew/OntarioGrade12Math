6ce39655158317c265cbe5cc15dd2d1a5c222943
Wonjae Lee <84299688+Sam-Wonjae-Lee@users.noreply.github.com>
2023-05-01 00:42:17 -0400

    Add files via upload

--- unit1.py
+++ unit1.py
@@ -7,7 +7,9 @@ This module contains code for Unit 1 functions
 
 import random
 import typing
+import sympy
 from sympy.abc import x
+
 
 # from sympy import symbols, Function, Symbol
 # from sympy.plotting import plot
@@ -16,7 +18,6 @@ from sympy.abc import x
 def generate_polynomial(degree: int, coefficient_range: typing.Tuple[int, int]):
     """
     Generates polynomial function
-
     Preconditions:
     - degree > 0
     - coefficient_range != ()
@@ -28,7 +29,20 @@ def generate_polynomial(degree: int, coefficient_range: typing.Tuple[int, int]):
         x_exponent -= 1
     return expr
 
+
 # TODO: Generate factorable polynomial and/or prefactored polynomial
+# Disciminatn is defined differently for different degree functions
+# So we might need to put a limit to degree
+# def generate_factorable_polynomial(degree: int, coefficient_range: typing.Tuple[int, int]):
+#     """
+#     Generates factorable polynomial.
+#     Will just return a non-factored polynomial if the generated
+#
+#     Preconditions:
+#     - degree > 1
+#     - coefficient_range != ()
+#     """
+#     possible_factorable = sympy.factor(generate_polynomial(degree, coefficient_range))
 
 
 def sympy_to_mathjax(polynomial) -> str:
@@ -46,7 +60,8 @@ def sympy_to_mathjax(polynomial) -> str:
     x = txt.replace("**", "^")
     y = x.replace("*", "")
 
-    return (y)
+    # return (y)
+    return y
 
 
 def end_behaviour(first_quadrant: str, second_quadrant: str, coefficient_range: typing.Tuple[int, int]):
@@ -100,6 +115,25 @@ def end_behaviour(first_quadrant: str, second_quadrant: str, coefficient_range: 
 # TODO: coefficent of given equation
 
 # TODO: domain range of given equation
+def function_domain(polynomial) -> sympy.polys.domains.integerring.IntegerRing:
+    """
+    Returns domain of polynomial
+
+    Preconditions:
+    - polynomial is a valid polynomial function from <generate_polynomial>
+    """
+    p = sympy.Poly(polynomial)
+    return p.domain
+
+
+def function_range(polynomial) -> sympy.polys.domains.integerring.IntegerRing:
+    """
+    Returns range of polynomial
+
+    Preconditions:
+    - polynomial is a valid polynomial function from <generate_polynomial>
+    """
+
 
 # TODO: table of intervals of given equation
 
@@ -108,6 +142,26 @@ def end_behaviour(first_quadrant: str, second_quadrant: str, coefficient_range: 
 # TODO: number of turning points of given equation
 
 # TODO: intercepts of given function
+
+def x_int(polynomial):
+    """
+    Returns x-intercept of polynomial
+
+    Preconditions:
+    - polynomial is a valid polynomial function from <generate_polynomial>
+    """
+    return sympy.solveset(polynomial, x)
+
+
+def y_int(polynomial):
+    """
+    Returns y-intercept of polynomial
+
+    Preconditions:
+    - polynomial is a valid polynomial function from <generate_polynomial>
+    """
+    return [polynomial.coeff(x, 0)]  # return just the y-intercept
+
 
 # TODO: least possible degree of given function ie. degree of polynomial function
 
