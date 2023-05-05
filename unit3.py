d17b22b124432006a921120d8707c2004ab3e13d
Calvin-Liew <calvin.y.liew@gmail.com>
2023-05-05 17:04:59 -0400

    Unit3 Planning

---
+++ unit3.py
@@ -0,0 +1,48 @@
+# Unit 3 Planning 
+
+#Generate rational functions 
+
+#TODO: Generate any rational function ie. functions with no asymptotes, functions with horzontal asymptote, etc
+
+#TODO: Generate quadractic rational functions 
+
+#TODO: Generate rational function with asymptotes so we just need to have the denominator be a factorable polynomial
+
+#TODO: Generate rational function that has one as the numerator and the deonominator be a simple factorable polynomial for questions asking about what the asymptotes are 
+
+#TODO: Intervals where function is positive ornegative 
+
+#TODO: Intercepts of rational function ie. when the numerator = 0. 
+
+#TODO: Domain and range of rational function 
+
+#TODO: graph of rational function 
+
+#TODO: Intervals where the function is increasing/decreasing ie. where the deriviative is above zero and below zero
+
+#TODO: The vertical asympotes of a rational function 
+
+#TODO: The horizonal asympotes of a rational function. Ex. As x-> infinity or x->-infinity y->above 0 or below 0 
+
+#TODO: The y-intercept, and x-intercepts of a rational function 
+
+#TODO: important attributes of rational functions (for graphing questions), ie. x-int, y-int, HA, VA, 
+
+#TODO: Generate quotient of linear functions 
+
+#TODO: Generate spceial case function: A function with holes 
+
+#TODO: Generate a rational fucnction with a oblique asymptote. CASE 1. A quadractic over a linear 
+
+#TODO: Generate a rational fucnction with a oblique asymptote. CASE 2. A higher order function over a higher ordeer fucntion
+# Have to solve where the function intersects the HA to graph 
+
+#TODO: Inequalities of rational functions 
+
+#TODO: Solving rational equations 
+
+
+
+
+
+
