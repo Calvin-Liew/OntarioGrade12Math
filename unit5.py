cbbec10f7951f6095cd59083731e6b71e65c2313
Calvin-Liew <calvin.y.liew@gmail.com>
2023-05-20 19:31:25 -0400

    unit 5 todos not fully compelted

---
+++ unit5.py
@@ -0,0 +1,33 @@
+# TODOS:
+
+# Remember period, cycle, amp
+
+# TODO: Generate tan, cos, sinx graph equation 
+
+# TODO: The same as above but with clear transformations similar to unit 1 transfromations
+
+# TODO: characteristics of a trig function (cos/tan/sin)
+# Provide attributes of ammplitude, max, min, axis of the curve, 
+
+# TODO: Give a equation of graph and one cycle (give different quadrants)
+
+# TODO: characteristics of a trig function (cos/tan/sin)
+# Provide attributes of ammplitude, max, min, axis of the curve, PLUS DOMAIN AND RANGE
+
+# TODO: Characterisitcs of reciproocl graphs, domain, range
+
+# TODO: some word problems of trig functions
+
+# TODO: transformations of trig functions stuff
+
+# TODO: Solving trig functions. SO we have to generate some random 
+# trig equations first 
+
+# TODO: IROC, AROC of trig functions
+
+# TODO: trig proving stuff
+
+
+
+
+
